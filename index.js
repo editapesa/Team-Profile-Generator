5dfd8319de56776921069e834c8fb4f760868f27
Edita Pesa <editapesa@gmail.com>
2021-04-08 17:31:55 -0400

    add somw emp questions and functions to index.js

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const Employee = require('./lib/employee');
 
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+
+
+const questions = [
+    {
+        type: 'input',
+        message: "What is the team member's name?",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the team member's ID number?",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "What is the team member's email?",
+        name: 'email',
+    }
+];
+
+// ??? need to add mgr, eng, intern questions
+
+
+// ??? function to write html, init app, call to init app??
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
+}
+
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        writeToFile('index.html', Employee({...answers}))
+    })
+}
+
+init();
 
 
 
