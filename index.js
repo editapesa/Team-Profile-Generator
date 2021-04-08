97d44e90fd4d0d7e62c67f60ad9501e87ed26d63
Edita Pesa <editapesa@gmail.com>
2021-04-08 16:43:16 -0400

    in lib folder, create spearate js files for each class

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
+const inquirer = require('inquirer');
+
+const Employee = require('./lib/employee');
 
 
-class Employee {
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
 
-    // add following methods
-    getName()
-    getId()
-    getEmail()
-    getrole()   // returns 'Employee'
-}
 
-class Manager extends Employee {
-    constructor(name, id, email, officeNumber) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.officeNumber = officeNumber;
-    }
 
-    getRole()  // returns 'Manager'
-}
 
-class Engineer extends Employee {
-    constructor(name, id, email, gitHub) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.gitHub = gitHub;
-    }
 
-    getGithub()
-    getRole()  // returns 'Engineer'
-}
 
-class Intern extends Employee {
-    constructor(name, id, email, school) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.school = school;
-    }
 
-    getSchool()
-    getRole()  // returns 'Intern"
-}
     
     
 
