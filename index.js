902434374b1400c933b6b2b59e4c444e1695d244
Edita Pesa <editapesa@gmail.com>
2021-04-10 15:17:05 -0400

    change questions to mgr first

--- index.js
+++ index.js
@@ -10,66 +10,107 @@ const Intern = require('./lib/intern');
 inquirer.prompt([
     {
         type: 'input',
-        message: "What is the team member's name?",
-        name: 'name',
+        message: "Enter the team manager's name.",
+        name: 'mgrName',
+    },
+    {
+        type: 'input',
+        message: "Enter the team manager's ID number.",
+        name: 'mgrId',
+    },
+    {
+        type: 'input',
+        message: "Enter the team manager's email.",
+        name: 'mgrEmail',
+    },
+    {
+        type: 'input',
+        message: "Enter the team manager's office number.",
+        name: 'officeNbr',
     },
     {
         type: 'list',
-        message: 'What is their role?',
-        name: 'role',
-        choices: ['Manager', 'Engineer', 'Intern'],
+        message: 'What would you like to do next?',
+        name: 'nextStep',
+        choices: ['Add an engineer', 'Add an intern', 'Finish building team'],
     },
-    {
-        type: 'input',
-        message: "What is the team member's ID number?",
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: "What is the team member's email?",
-        name: 'email',
-    }
 ])
     
 .then((response) => {
-        if (response.role === 'Engineer')  {
-            getGithub();
-        }else if (response.role === 'Intern') {
-            getSchool();
+        if (response.nextStep === 'Add an engineer')  {
+            addEngineer();
+        }else if (response.nextStep === 'Add an intern') {
+            addIntern();
         }else {
-            getOfficeNumber();
+            finishTeam();
         }
     });
 
 
-function getGithub() {
+function addEngineer() {
         inquirer.prompt([
+            {
+                type: 'input',
+                message: "Enter the engineer's name.",
+                name: 'engName',
+            },
+            {
+                type: 'input',
+                message: "Enter the engineer's ID number.",
+                name: 'engId',
+            },
+            {
+                type: 'input',
+                message: "Enter the engineer's email.",
+                name: 'engEmail',
+            },
             {
                 type: 'input',
                 message: "What is their GitHub profile username?",
                 name: 'github',
-            }
+            },
+            {
+                type: 'list',
+                message: 'What would you like to do next?',
+                name: 'nextStep',
+                choices: ['Add an engineer', 'Add an intern', 'Finish building team'],
+            },
         ])
     };
 
-function getSchool() {
+function addIntern() {
         inquirer.prompt([
+            {
+                type: 'input',
+                message: "Enter the intern's name.",
+                name: 'intName',
+            },
+            {
+                type: 'input',
+                message: "Enter the intern's ID number.",
+                name: 'intId',
+            },
+            {
+                type: 'input',
+                message: "Enter the intern's email?",
+                name: 'intEmail',
+            },
             {
                 type: 'input',
                 message: "Which school do they attend?",
                 name: 'school',
-            }
+            },
+            {
+                type: 'list',
+                message: 'What would you like to do next?',
+                name: 'nextStep',
+                choices: ['Add an engineer', 'Add an intern', 'Finish building team'],
+            },
         ])
     };
 
-function getOfficeNumber() {
-        inquirer.prompt([
-            {
-                type: 'input',
-                message: "What is their office number?",
-                name: 'officeNumber',
-            }
-        ])
+function finishTeam() {
+        
     };
 
 // .then(answers => {
