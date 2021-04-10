5d4aeea4e01e684ade42616924d3b433e5f7c64f
Edita Pesa <editapesa@gmail.com>
2021-04-09 23:41:28 -0400

    add role prop, mrg/intern/eng questions. gh/school/office# functions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Team-Profile-Generator
+# Team Profile Generator
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+
+## Installation
+In the code editor, select the folder to use and open the Integrated Terminal (or GitBash). From there run `npm init`, followed by `npm i inquirer`, and `npm i jest -D`. The application can then be started by entering the command `node index.js`.
+
+## License
+This project is licensed under the MIT license.
+[MIT License](https://opensource.org/licenses/MIT)
+
+## Usage
+Click the link below to see a video of how the application works.
+
+[Team Profile Gen](https://url.com)
+
+![demo pic 1](demo-pic1.jpg)
+
+![demo pic 2](demo-pic2.jpg)
--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-
-const questions = [
+inquirer.prompt([
     {
         type: 'input',
         message: "What is the team member's name?",
         name: 'name',
+    },
+    {
+        type: 'list',
+        message: 'What is their role?',
+        name: 'role',
+        choices: ['Manager', 'Engineer', 'Intern'],
     },
     {
         type: 'input',
@@ -24,23 +29,67 @@ const questions = [
         message: "What is the team member's email?",
         name: 'email',
     }
-];
+])
+    
+    // ??? need to add mgr, eng, intern questions
+.then((response) => {
+        if (response.role === 'Engineer')  {
+            getGithub();
+        }else if (response.role === 'Intern') {
+            getSchool();
+        }else {
+            getOfficeNumber();
+        }
+    });
 
-// ??? need to add mgr, eng, intern questions
+
+function getGithub() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "What is their GitHub profile username?",
+                name: 'github',
+            }
+        ])
+    };
+
+function getSchool() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "What is their school name?",
+                name: 'school',
+            }
+        ])
+    };
+
+function getOfficeNumber() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "What is their office number?",
+                name: 'officeNumber',
+            }
+        ])
+    };
+
+// .then(answers => {
+//     fs.writeFile('index.html', Employee({...answers}))
+// })
 
 
 // ??? function to write html, init app, call to init app??
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(fileName, data);
-}
+// function writeToFile(fileName, data) {
+//     return fs.writeFileSync(fileName, data);
+// }
 
-function init() {
-    inquirer.prompt(questions).then(answers => {
-        writeToFile('index.html', Employee({...answers}))
-    })
-}
+// function init() {
+//     inquirer.prompt(questions).then(answers => {
+//         writeToFile('index.html', Employee({...answers}))
+//     })
+// }
 
-init();
+// init();
 
 
 
