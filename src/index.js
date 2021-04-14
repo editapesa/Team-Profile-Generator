57a5a3317fc1a2e215794e1a850ccbc83222a249
Edita Pesa <editapesa@gmail.com>
2021-04-14 13:48:14 -0400

    move js file to src folder & html to dist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const Employee = require('./lib/employee');
+const Employee = require('../lib/employee');
 
-const Manager = require('./lib/manager');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
+const Manager = require('../lib/manager');
+const Engineer = require('../lib/engineer');
+const Intern = require('../lib/intern');
 
 const generateHtml = (answers) =>
     `<!DOCTYPE html>
